2cb60bbaa02d101255d22e246b1b572b7c4f14ef
aswin-vignesh <80969724+aswin-vignesh@users.noreply.github.com>
2023-08-31 17:18:59 +0530

    Update apiSlice.js Backend Link

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -4,7 +4,7 @@ import {setCredentials} from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
     // baseUrl : 'http://localhost:3500',
-    baseUrl : 'https://av-technote-api.onrender.com',
+    baseUrl : 'https://av-technote-api.onrender.com/',
     credentials : 'include',
     prepareHeaders : (headers, {getState}) => {
         const token = getState().auth.token
